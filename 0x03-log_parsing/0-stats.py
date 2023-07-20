3c6d99eab03498db8a35b87a25e1e9bb2de4abb6
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-19 22:55:22 -0700

    Added Comments

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python3
+"""This File Solves Log Parsing """
+
 import sys
 
+
 def print_stats(total_size, status_counts):
+    """ Print the total file size"""
+
     print("Total file size:", total_size)
+
+    """ Sort the status codes in ascending order """
     sorted_statuses = sorted(status_counts.keys())
+
+    """ Print the number of lines for each status code """
     for status in sorted_statuses:
         count = status_counts[status]
         print("{}: {}".format(status, count))
 
+
 def main():
+	"""Main Function that executes print_stats """
     total_size = 0
     status_counts = {}
 
@@ -16,9 +28,9 @@ def main():
         for line in sys.stdin:
             line_counter += 1
 
-            # Split the line by space and validate the format
+            """ Split the line by space and validate the format"""
             parts = line.split()
-            if len(parts) != 10 or parts[2] != "GET" or parts[4] != "HTTP/1.1":
+            if len(parts) != 9 or parts[3] != "\"GET" or parts[5] != "HTTP/1.1\"":
                 continue
 
             try:
@@ -27,14 +39,19 @@ def main():
             except (ValueError, IndexError):
                 continue
 
+            """ Accumulate the total file size """
             total_size += file_size
+
+            """ Update the count for each status code """
             status_counts[status_code] = status_counts.get(status_code, 0) + 1
 
             if line_counter % 10 == 0:
+                """ Print statistics after every 10 lines """
                 print_stats(total_size, status_counts)
-                print("\n")
+                print()
 
     except KeyboardInterrupt:
+        """ Print final statistics in case of keyboard interruption"""
         print_stats(total_size, status_counts)
 
 if __name__ == "__main__":
